fe8cb792c43cddce37a795c63dc2d99f65095981
Tony Lee <hy.tony.lee@gmail.com>
2019-06-07 20:29:08 -0700

    finish redux setup in app.js

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,41 @@ import ReactDOM from 'react-dom';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
-import AppRouter from './routers/AppRouter'
+//Redux
+import configureStore from './store/configureStore';
+import { addPost } from './actions/posts';
+import { setTextFilter } from './actions/filters';
+import getVisiblePosts from './selectors/post';
+
+//Routers
+import AppRouter from './routers/AppRouter';
+
+
+const store = configureStore();
+
+const PostOne = store.dispatch(addPost({
+    desc: 'Ramen Kiosk',
+    postType: 'portfolio',
+    note: 'POS Web application with react frontend and Rail backend.',
+    link: 'https://github.com/hytonylee/Ramen-Kiosk',
+    createdAt: 1000
+}))
+
+const postTwo = store.dispatch(addPost({
+    desc: 'Swift Converter',
+    postType: 'portfolio',
+    note: 'Swift format upload and convert to readable csv in MERN stack.',
+    link: 'https://github.com/hytonylee/Swift-Converter',
+    createdAt: -1000
+}));
+
+store.dispatch(setTextFilter('react'));
+
+// store.subscribe(() => {
+const state = store.getState();
+const visiblePosts = getVisiblePosts(state.posts, state.filters)
+console.log(visiblePosts);
+// });
 
 ReactDOM.render(<AppRouter />, document.getElementById('app'));
 
