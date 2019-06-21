7ea3f83090fb29416e24d6dc7d94138d2db94e92
Tony Lee <hy.tony.lee@gmail.com>
2019-06-20 18:08:08 -0700

    Clean code in firebase.js'

--- src/app.js
+++ src/app.js
@@ -13,42 +13,10 @@ import { Provider } from 'react-redux';
 //Routers
 import AppRouter from './routers/AppRouter';
 
+//Firebase
 import './firebase/firebase';
 
-
-
 const store = configureStore();
-
-const PostOne = store.dispatch(addPost({
-    title: 'Ramen Kiosk',
-    postType: 'portfolio',
-    note: 'POS Web application with react frontend and Rail backend.',
-    link: 'https://github.com/hytonylee/Ramen-Kiosk',
-    createdAt: 1000,
-}))
-
-const postTwo = store.dispatch(addPost({
-    title: 'Swift Converter',
-    postType: 'portfolio',
-    note: 'Swift format upload and convert to readable csv in MERN (react) stack.',
-    link: 'https://github.com/hytonylee/Swift-Converter',
-    createdAt: -1000,
-}));
-
-const postThree = store.dispatch(addPost({
-    title: 'Journal 1',
-    postType: 'blog',
-    note: 'Writing on how to build React with Apollo.',
-    link: 'https://github.com/hytonylee/Swift-Converter',
-    createdAt: 100,
-}));
-
-// store.dispatch(setTextFilter('react'));
-
-// setTimeout(() => {
-//     store.dispatch(setTextFilter('swift'))
-// }, 3000)
-
 const state = store.getState();
 const visiblePosts = getVisiblePosts(state.posts, state.filters)
 console.log(visiblePosts);
--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -11,9 +11,7 @@ const firebaseConfig = {
 };
 
 firebase.initializeApp(firebaseConfig);
-
 const database = firebase.database();
-
 
 export { firebase, database as default }
 
